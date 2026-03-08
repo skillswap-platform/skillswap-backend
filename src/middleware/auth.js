0a55256349819e3fec3597443fd5aae47a020564
Aditya Ajmire <ajmireaditya733@gmail.com>
2026-03-08 10:26:34 +0530

    Fixed Auth Issue

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,16 +18,14 @@ const protect = async (req, res, next) => {
       // Get user from the token
       req.user = await User.findById(decoded.id).select('-password');
 
-      next();
+      return next();
     } catch (error) {
       console.error(error);
-      res.status(401).json({ error: 'Not authorized, token failed' });
+      return res.status(401).json({ error: 'Not authorized, token failed' });
     }
   }
 
-  if (!token) {
-    res.status(401).json({ error: 'Not authorized, no token' });
-  }
+  return res.status(401).json({ error: 'Not authorized, no token' });
 };
 
 export default protect;
